8c7681777b096f5d242d793c69e6406354f0ac18
Alejandro Aguilar Higuera <alejandro.aguilar.higuera@gmail.com>
2026-05-03 10:58:21 -0700

    add robots and sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,19 @@
+// app/sitemap.ts
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://alexaguilar.dev',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    // {
+    //   url: 'https://alexaguilar.dev/proyectos',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'weekly',
+    //   priority: 0.8,
+    // },
+  ]
+}
